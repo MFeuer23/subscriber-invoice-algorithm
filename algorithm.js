6d5df7960a7d8c090c7a45868faf93ff0655acdd
Mallory Feuer <mallory.feuer@gmail.com>
2019-06-12 18:29:48 +0000

    add dot notation and semi colons

--- algorithm.js
+++ algorithm.js
@@ -3,11 +3,11 @@ function cancellationNotice(sub, inv) {
   let overdueIds = [];
 
   for (let i = 0; i < inv.length; i++) {
-    if (today > new Date(inv[i]["due_date"]) && !inv[i]["payment_date"]) {
-      overdueIds.push(inv[i]["subscriber_id"])
+    if (today > new Date(inv[i].due_date) && !inv[i].payment_date) {
+      overdueIds.push(inv[i].subscriber_id);
     }
   }
   
-  let overdueSubscribers = sub.filter(subscriber => overdueIds.includes(subscriber.id))
-  return overdueSubscribers
+  let overdueSubscribers = sub.filter(subscriber => overdueIds.includes(subscriber.id));
+  return overdueSubscribers;
 }
